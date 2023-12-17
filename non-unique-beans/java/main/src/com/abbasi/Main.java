bdbe33055658751d12b2fad646a095b12854b9c7
mohammad Abbasi <mohammed@virtugrp.com>
2023-12-17 13:25:46 +0330

    NonUniqueBeanDefinition with method name of beans

---
+++ non-unique-beans/java/main/src/com/abbasi/Main.java
@@ -0,0 +1,21 @@
+package com.abbasi;
+
+import com.abbasi.config.ProjectConfig;
+import com.abbasi.model.Vehicle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //get bean from the name of bean
+        //the name of the bean is the name of the method
+        var audiVehicle = context.getBean("vehicle1",Vehicle.class);
+        var benzVehicle = context.getBean("vehicle2",Vehicle.class);
+        var ferrariVehicle = context.getBean("vehicle3",Vehicle.class);
+        System.out.println("Audi vehicle is: "+ audiVehicle.getName());
+        System.out.println("Benz vehicle is: "+ benzVehicle.getName());
+        System.out.println("Ferrari vehicle is: "+ ferrariVehicle.getName());
+    }
+}
---
+++ non-unique-beans/java/main/src/com/abbasi/config/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.abbasi.config;
+
+import com.abbasi.model.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Vehicle vehicle1(){
+        var vehicle = new Vehicle();
+        vehicle.setName("Audi8");
+
+        return vehicle;
+    }
+
+    @Bean
+    public Vehicle vehicle2(){
+        var vehicle = new Vehicle();
+        vehicle.setName("Benz");
+
+        return vehicle;
+    }
+
+
+    @Bean
+    public Vehicle vehicle3(){
+        var vehicle = new Vehicle();
+        vehicle.setName("Ferrari");
+
+        return vehicle;
+    }
+}
