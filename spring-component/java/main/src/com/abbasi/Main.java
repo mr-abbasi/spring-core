8eb3a3123272f0b2c7ddb36ce8898c2aec6a2c01
mohammad Abbasi <mohammed@virtugrp.com>
2023-12-17 17:57:31 +0330

    creating bean with component stereotype

---
+++ spring-component/java/main/src/com/abbasi/Main.java
@@ -0,0 +1,15 @@
+package com.abbasi;
+
+import com.abbasi.config.ProjectConfig;
+import com.abbasi.model.Vehicle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Creating Vehicle with Component stereotype");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle bean is created with Component Stereotype: "+vehicle.getName());
+        vehicle.printHello();
+    }
+}
---
+++ spring-component/java/main/src/com/abbasi/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.abbasi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.abbasi.model"})
+public class ProjectConfig {
+
+}
